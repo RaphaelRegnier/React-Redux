0ebf70673f67854f9cf5dab97199f88fc690dcbe
Raphael Regnier <raphael.regnier@hotmail.fr>
2018-05-25 19:32:57 +0200

    preparing

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'; //to be able to write jsx
+import ReactDOM from 'react-dom'; //to render
+
+import Hello from './tuto_before_exercise.jsx'
+import '../assets/stylesheets/application.scss';
+
--- src/tuto_before_exercise.jsx
+++ src/tuto_before_exercise.jsx
@@ -39,3 +39,5 @@ const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(<Hello name="Raphael" />, root);
 }
+
+export default Hello
